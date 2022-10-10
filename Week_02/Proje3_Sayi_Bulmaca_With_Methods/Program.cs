3d756c8914924d327cf9e73a20c11ae5aae208e7
Melahatsurmez <srmzmelahat123@gmail.com>
2022-10-10 18:18:53 +0300

    10.10.2022

--- Week_02/Proje3_Sayi_Bulmaca_With_Methods/Program.cs
+++ Week_02/Proje3_Sayi_Bulmaca_With_Methods/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Sayı bulmaca oyununu yeni öğrendiğmiz methods konusu bağlamında yeniden hazırlayınız.
+            /*
+             * 5 tercih hakkı olsun
+             * Puan sistemi olsun (1 tercihte bildiyse 50, bimediyse 10'ar 10'ar düşsün.)
+             * Oyunu kaybettiğinde ya da kazandığında Yeniden Oynamak İstiyor ise Oynayabilsin!
+             */
+
+            string tercih;
+            do
+            {
+                Console.Clear();
+                //Oyun.Oyna();
+                do
+                {
+                    //Console.Clear();
+                    Console.Write("Yeniden oynamak ister misiniz?[E/H]");
+                    tercih = Console.ReadLine().ToUpper();
+                } while (tercih != "H" && tercih != "E");
+            } while (tercih != "H");
+            Console.ReadLine();
         }
     }
 }
