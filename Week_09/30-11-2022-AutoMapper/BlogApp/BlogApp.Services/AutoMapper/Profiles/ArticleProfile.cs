18d20b8e5943e8f04c031cac76285b1cb68607c2
Melahatsurmez <srmzmelahat123@gmail.com>
2022-11-30 18:15:01 +0300

    30.11.2022

---
+++ Week_09/30-11-2022-AutoMapper/BlogApp/BlogApp.Services/AutoMapper/Profiles/ArticleProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using BlogApp.Entities.Concrete;
+using BlogApp.Entities.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApp.Services.AutoMapper.Profiles
+{
+    public class ArticleProfile : Profile
+    {
+        public ArticleProfile()
+        {
+            //İstersek katerogori içinde ayrı oluşturmamak için baseProfile olulturup ikisinide birlikte yazabilirdik.
+            CreateMap<ArticleAddDto, Article>()
+                .ForMember(destination=>destination.CreatedDate,option=>option.MapFrom(x=>DateTime.Now));
+            CreateMap<ArticleUpdateDto, Article>()
+                .ForMember(destination=>destination.ModifiedDate,option=>option.MapFrom(x=> DateTime.Now));
+        }
+    }
+}
