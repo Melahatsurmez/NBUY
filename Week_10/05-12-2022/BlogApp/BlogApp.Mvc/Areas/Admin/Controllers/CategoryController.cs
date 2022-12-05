5d82e720eb09f4ca0400264f8717004c093a3432
Melahatsurmez <srmzmelahat123@gmail.com>
2022-12-05 18:15:08 +0300

    05.12.2022

---
+++ Week_10/05-12-2022/BlogApp/BlogApp.Mvc/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,78 @@
+﻿using BlogApp.Entities.Dtos;
+using BlogApp.Mvc.Areas.Admin.Models;
+using BlogApp.Services.Abstract;
+using BlogApp.Shared.Utilities.Extensions;
+using BlogApp.Shared.Utilities.Result.ComplexTypes;
+using BlogApp.Shared.Utilities.Result.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace BlogApp.Mvc.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var result = await _categoryService.GetAllByNonDeleted();
+            if (result.ResultStatus == ResultStatus.Success)
+            {
+                return View(result.Data);
+            }
+            return View();
+        }
+        public IActionResult Add()
+        {
+            return PartialView("_CategoryAddPartial");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(CategoryAddDto categoryAddDto)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _categoryService.Add(categoryAddDto, "Melahat Sürmez");
+                if (result.ResultStatus == ResultStatus.Success)
+                {
+                    var categoryAddAjaxModel = JsonSerializer.Serialize(new CategoryAddAjaxViewModel
+                    {
+                        CategoryDto = result.Data,
+                        CategoryAddPartial = await this.RenderViewToStringAsync("_CategoryAddPartial", categoryAddDto)
+                    });
+                    return Json(categoryAddAjaxModel);
+                }
+            }
+
+            var categoryAddAjaxErrorModel = JsonSerializer.Serialize(new CategoryAddAjaxViewModel
+            {
+                CategoryAddPartial = await this.RenderViewToStringAsync("_CategoryAddPartial", categoryAddDto)
+            });
+            return Json(categoryAddAjaxErrorModel);
+        }
+        public async Task<JsonResult> GetAllCategories()
+        {
+            var result =await  _categoryService.GetAllByNonDeleted();
+            //json a dönüştürülmüş halini içinde barındırıcak bir değişken tanımlıyoruz (parantez içinde dönüştürmek istediğimiz değer var)
+            var resultJson=JsonSerializer.Serialize(result.Data, new JsonSerializerOptions //data gönderildiğinde genelde new komutu kullanılır.
+            {
+                ReferenceHandler=ReferenceHandler.Preserve
+            });
+            return Json(resultJson); // böylece json tipinde kategori bilgisi verilecek
+        }
+        [HttpPost]
+        public async Task<JsonResult> Delete(int categoryId)
+        {
+            var result = await _categoryService.Delete(categoryId, "Melahat Sürmez");
+            var resultJson = JsonSerializer.Serialize(result);
+            return Json(resultJson);
+        }
+    }
+}
