2e4dda15bd95a06a2e74ee2f5ea5b193d51dbde8
Melahatsurmez <srmzmelahat123@gmail.com>
2022-12-19 18:03:07 +0300

    19.12.2022

---
+++ Week_12/19-12-2022-ödev/ShoppingApp/ShoppingApp.Web/Models/Dtos/ConfirmDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ShoppingApp.Web.Models.Dtos
+{
+    public class ConfirmDto
+    {
+        [DisplayName("Email Adresi")]
+        [Required(ErrorMessage ="{0} alanı zorunludur.")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+    }
+}
