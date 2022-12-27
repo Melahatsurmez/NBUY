458dd6caa7430296973c05d5ceff34c9c8320c05
Melahatsurmez <srmzmelahat123@gmail.com>
2022-12-27 18:07:03 +0300

    27.12.2022

---
+++ Week_13/27-12-2022/ShoppingApi/ShoppingApi.API/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShoppingApi.Business.Abstract;
+
+namespace ShoppingApi.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        //Projenizdde swagger var ise tüm  action mettolarınızın Http Request türü yazılmalıdır.HttpGet, HttpPost vb.
+        [HttpGet]
+        public async Task<IActionResult> GetProducts() 
+        {
+            var product = await _productService.GetAllAsync();
+            return Ok(product);
+        }
+    }
+}
